47f06af41ac7c8e192542a2405ba675fc8772c47
XEverentX <Inok.Alex@gmail.com>
2018-11-27 00:42:39 +0300

    first commit

---
+++ build.sh
@@ -0,0 +1,14 @@
+python scripts/lint.py ${PWD}
+
+if [ ! -d "build" ]; then
+mkdir build
+fi
+
+cd build
+cmake -D CMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
+cppcheck -j4 —project=compile_commands.json &> log_cppcheck
+python ../scripts/static_analysis.py log_cppcheck
+make -j4
+./tests/Test_Target
+./modules/application/Graph_Partition
+cd ..
