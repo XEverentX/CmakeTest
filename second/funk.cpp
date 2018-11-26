af9478391422f39805f96456696bbb862c446d7a
XEverentX <Inok.Alex@gmail.com>
2018-11-26 20:47:07 +0300

    all

---
+++ second/funk.cpp
@@ -0,0 +1,5 @@
+#include<iostream>
+
+int f (int x, int y) {
+	return x + y;
+}
---
+++ second/main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include"funk.h"
+
+int main() {
+	int x, y;
+	std::cin >> x >> y;
+	int ans = f (x, y);
+	std::cout << ans << std::endl;
+	return 0;
+}
