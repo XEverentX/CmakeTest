945b199d3b661713a95b88ae620af3512bcde4f8
XEverentX <Inok.Alex@gmail.com>
2018-11-27 22:22:37 +0300

    Complite second cmake dir

---
+++ second/bui/CMakeFiles/funk.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/funk.dir/funk.cpp.o"
+  "libfunk.pdb"
+  "libfunk.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/funk.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ second/build.sh
@@ -0,0 +1,6 @@
+mkdir build
+cd build
+
+cmake ~/space/CmakeTest/second/
+make
+cd ..
